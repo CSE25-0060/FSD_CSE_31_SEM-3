3e50959f84fef9089d9837428f441ff1b385c36f
Sanskriti <sanskriti.1837@gmail.com>
2026-08-03 14:20:28 +0530

    Added files

---
+++ Javascript/script.js
@@ -0,0 +1,86 @@
+//Ways to display data in JavaScript.
+// console.log("Hello World!");
+// document.write("Hello Javascript!");
+// window.alert("YOUUUUUUUUUU!");
+
+
+//LET keyword functioning.
+// let a="SANSKRITI";
+// console.log("The value of a is:",a);
+// console.log(typeof a);
+
+// let a=34;
+// let b=50;
+// let sum=a+b;
+// console.log("The sum of a and b is:",sum);
+
+//WAP to take two numbers from user and perform all arithmetic operations on them.
+// let a = prompt("Enter the value of a:");
+// let b = prompt("Enter the value of b:");
+// let c = Number(a) + Number(b);
+// let d = Number(a) - Number(b);
+// let e = Number(a) * Number(b);
+// let f = Number(a) / Number(b);
+// let g = Number(a)%Number(b);
+// let h = Number(a)**Number(b);
+// let i = Number(a)++;
+// let j = Number(a)--;
+// let k = Number(b)++;
+// let l = Number(b)--;
+// document.write("The sum of a and b is:" + c);
+// document.write ("The difference of a and b is:" + d);
+// document.write("<br>" + "The product of a and b is:" + e);
+// document.write("<br>" + "The division of a and b is:" + f);
+// document.write("<br>" + "The remainder of a and b is:" + g);
+// document.write("<br>" + "The power of a and b is:" + h);
+// document.write("<br>" + "The increment of a is:" + i);
+// document.write("<br>" + "The decrement of a is:" + j);
+// document.write("<br>" + "The increment of b is:" + k);
+// document.write("<br>" + "The decrement of b is:" + l);
+
+//Boolean data type in JavaScript.
+// let a=true;
+// console.log(typeof a);
+// let b=false;
+// console.log(typeof b);
+
+//Big Int data type in JavaScript.
+// let c=Number.MAX_SAFE_INTEGER;
+// console.log("The maximum safe integer in JavaScript is:",c);
+// let d=Number.MIN_SAFE_INTEGER;
+// console.log("The minimum safe integer in JavaScript is:",d);
+// console.log(c+12);
+// console.log(c+20);
+
+// let e=BigInt(c);
+// console.log("The value of a is:",e);
+// let f=BigInt(1000);
+// let g=e+f;
+// console.log("The value of g is:",g);
+
+
+//Undefined data type in JavaScript.
+// let x;
+// console.log(typeof x);
+
+//Null data type in JavaScript.
+// let y=null;
+// console.log(typeof y);
+
+//Symbol data type in JavaScript.
+// let z=Symbol();
+// let w=Symbol();
+// console.log(z==w);
+// console.log(z===w);
+
+//Eligible to vote or not using if else statement in JavaScript.
+let age=prompt("Enter your age:");
+if (age>=18)
+{
+    document.write("You are eligible to vote.");
+}
+else
+{
+    document.write("You are not eligible to vote.");
+}
+
